d5e79fb5759c5480041944c6c8ae0c06ca4a8489
xiao hua <erixiao@cisco.com>
2020-04-03 12:18:41 +0800

    work fine for mpls

--- src/traffic.py
+++ src/traffic.py
@@ -1,18 +1,15 @@
-from socket import socket, AF_PACKET, SOCK_RAW, inet_aton
+from socket import socket, inet_aton, htons, AF_PACKET, SOCK_RAW
 import numpy as np
 import struct
-import pprint
+
+ids = []
+seq = 8760
+PROTO_MPLS = 0x8847
+PROTO_IPV4 = 0x0800
 
 
 def generate(number_of_bytes):
     return np.random.bytes(number_of_bytes)
-
-# save sent packet id
-ids = []
-seq = 1000
-payload_len = 1000
-PROTO_MPLS = 0x8847
-PROTO_IPV4 = 0x0800
 
 
 def eth_hdr(dst, src, proto=PROTO_IPV4):
@@ -27,7 +24,6 @@ def eth_hdr(dst, src, proto=PROTO_IPV4):
 
     return hdr
 
-
 def mpls_hdr(labels, ttl=63):
     ''' return mpls header '''
     mpls = []
@@ -37,8 +33,7 @@ def mpls_hdr(labels, ttl=63):
         o = (o << 8) | ttl
         mpls.append(o)
     
-    return struct.pack(f'!{len(mpls)}I', *mpls)
-
+    return struct.pack(f'!{len(labels)}I', *mpls)
 
 def ip_payload(src=None, dst=None, payload=None, ttl=63):
     ''' return ip header
@@ -46,50 +41,50 @@ def ip_payload(src=None, dst=None, payload=None, ttl=63):
     dscp: 0, ecn: 0
     total_len: payload_len + hdr_len
     id: 2bytes
+
     '''
     if src is None:
         src = int.from_bytes(generate(4), byteorder='big')
     else:
-        src = inet_aton(src)
+        src = int.from_bytes(inet_aton(src), 'big')
 
     if dst is None:
         dst = int.from_bytes(generate(4), byteorder='big')
     else:
-        dst = inet_aton(dst)
+        dst = int.from_bytes(inet_aton(dst), 'big')
 
     if payload is None:
         payload = udp()
 
-    global seq
     checksum = 0
+    global seq
     hdr = struct.pack('!BBHHHBBHII', 0x45, 0x00, len(payload) + 20,
                 seq, 0x4000,
                 ttl, 0x11, checksum, 
                 src, dst)
-    checksum = chksum(hdr)
     hdr = struct.pack('!BBHHHBBHII', 0x45, 0x00, len(payload) + 20,
                 seq, 0x4000,
-                ttl, 0x11, checksum, 
+                ttl, 0x11, chksum(hdr), 
                 src, dst)
     seq += 1
     return hdr + payload    
 
 
-def udp(data=40000, src=40000, dst=None):
-    '''gen random bytes, src and dst is the port'''
-    #if src is None:
-    #    src = int.from_bytes(generate(2), byteorder='big')
-    #if dst is None:
-    #    dst = int.from_bytes(generate(2), byteorder='big')
+def udp(data=None, src=None, dst=None):
+    '''gen 1456 bytes '''
+    if src is None:
+        src = int.from_bytes(generate(2), byteorder='big')
+    if dst is None:
+        dst = int.from_bytes(generate(2), byteorder='big')
+    else:
+        dst = dst #htons(dst)
 
     if data is None:
-        data = generate(payload_len)
+        data = generate(100)
     
     checksum = 0
-    # skip checksum
     hdr = struct.pack('!4H', src, dst, len(data) + 8, checksum)
-    checksum = chksum(hdr)
-    hdr = struct.pack('!4H', src, dst, len(data) + 8, checksum)
+    hdr = struct.pack('!4H', src, dst, len(data) + 8, chksum(hdr))
     
     return hdr + data
 
@@ -104,15 +99,28 @@ def chksum(payload):
 
 
 if __name__ == '__main__':
-    addr = ('ens192', 0)
+    ifname = 'ens160'
+    # next hop 10.124.209.195 00:50:56:97:09:75
+    mac_src, mac_dst = '00:50:56:97:9a:7f', '00:50:56:97:8c:bc'
+    ip_src, ip_dst = '10.1.1.1', '192.2.235.235'
+    #paths = [17]
+    paths = [18215,17113,17014,17007,17111]
+    #17001/17003/16194
     num_to_send = 1
-    mac_src, mac_dst = '00:50:56:88:c9:54', '00:50:56:88:c9:51'
-    path_mpls_labels = [17003, 17011, 17113, 24004]
 
     s = socket(AF_PACKET, SOCK_RAW)
-    s.bind(addr)
-    sent = 0
-    while sent < num_to_send:
-        r = s.send(eth_hdr(mac_dst, mac_src, PROTO_MPLS) + mpls_hdr(path_mpls_labels) + ip_payload())
-        print('sent bytes: ', r)
-        sent += 1
+    s.bind((ifname, 0))
+
+    import time
+    num = 0
+    while num < num_to_send:
+        #packet = eth_hdr(mac_dst, mac_src, PROTO_IPV4) + \
+        packet = eth_hdr(mac_dst, mac_src, PROTO_MPLS) + \
+            mpls_hdr(paths) + \
+            ip_payload(ip_src, ip_dst, payload=udp(dst=4521))
+        r = s.send(packet)
+        print(f'{num+1} sent {r} bytes!')
+        num += 1
+        time.sleep(1)
+
+    print(f'Total sent {num_to_send}.')
